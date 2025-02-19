520c0ad2dd1d20018e15ad6386a5db3fd2c86c75
Adam Schafer <ams-tech@users.noreply.github.com>
2025-02-19 09:07:42 -0500

    rename complete

--- test/test_python_proto.py
+++ test/test_python_proto.py
@@ -5,8 +5,12 @@ from proto import greeting_pb2
 
 
 class TestCase(unittest.TestCase):
-    def test_message(self):
-        pass
+    def test_get_greet_encode_and_Decode(self):
+        my_request = greeting_pb2.GetGreet(
+            name="Adam"
+        )
+        self.assertIsNotNone(my_request)
+
 
 
 if __name__ == "__main__":
