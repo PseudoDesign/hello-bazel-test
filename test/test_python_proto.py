8a35dbfd68f5bf0651c4b8839bae74ad06dba0ab
Adam Schafer <ams-tech@users.noreply.github.com>
2025-02-19 09:15:31 -0500

    encoded and decoding

--- test/test_python_proto.py
+++ test/test_python_proto.py
@@ -5,11 +5,16 @@ from proto import greeting_pb2
 
 
 class TestCase(unittest.TestCase):
-    def test_get_greet_encode_and_Decode(self):
+    def test_get_greet_encode_and_decode(self):
         my_request = greeting_pb2.GetGreet(
             name="Adam"
         )
         self.assertIsNotNone(my_request)
+        serialized = my_request.SerializeToString()
+        decoded_request = greeting_pb2.GetGreet()
+        decoded_request.ParseFromString(serialized)
+        self.assertEqual("Adam", decoded_request.name)
+
 
 
 
