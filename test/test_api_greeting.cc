a3943eac9288e2e138028833df3a9105c837e8da
Adam Schafer <ams-tech@users.noreply.github.com>
2025-02-19 06:50:50 -0500

    deserialzied name

--- test/test_api_greeting.cc
+++ test/test_api_greeting.cc
@@ -1,3 +1,12 @@
 #include <gtest/gtest.h>
 #include "api/greeting.pb.h"
 
+TEST(ApiGreeting, EncodeAndDecode) {
+    greeting::GetGreet my_greeting, parsed_greeting;
+    std::string serialized;
+
+    my_greeting.set_name("Adam");
+    my_greeting.SerializeToString(&serialized);
+    parsed_greeting.ParseFromString(serialized);
+    EXPECT_EQ("Adam", parsed_greeting.name());
+}
