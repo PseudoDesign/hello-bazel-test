3914833abff0ea9fb70df0915faf10f05ea7eb0a
Adam Schafer <ams-tech@users.noreply.github.com>
2025-02-19 09:20:55 -0500

    encoding and decoding working

--- test/test_python_proto.py
+++ test/test_python_proto.py
@@ -15,7 +15,15 @@ class TestCase(unittest.TestCase):
         decoded_request.ParseFromString(serialized)
         self.assertEqual("Adam", decoded_request.name)
 
-
+    def test_get_greet_response_encode_and_decode(self):
+        my_response = greeting_pb2.GetGreetResponse(
+            greeting="Hello Adam"
+        )
+        self.assertIsNotNone(my_response)
+        serialized = my_response.SerializeToString()
+        decoded_response = greeting_pb2.GetGreetResponse()
+        decoded_response.ParseFromString(serialized)
+        self.assertEqual("Hello Adam", decoded_response.greeting)
 
 
 if __name__ == "__main__":
